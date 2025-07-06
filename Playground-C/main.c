d4a80b585598a17afc0b4388edb227cdaedbb3b6
Pavel Koyushev <pkoyushev@gmail.com>
2025-07-06 15:48:06 +0300

    Initial Commit

---
+++ Playground-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Playground-C
+//
+//  Created by Pavel Koyushev on 06.07.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
